cdbe1933b5d7bca708ac967f5fc2255474e6394b
Nicholas <nicholas.jacobs@maine.edu>
2021-08-19 22:20:28 -0400

    Added edge cases

--- problem4.py
+++ problem4.py
@@ -44,7 +44,6 @@ def is_user_in_group(user, group):
 
 grandparents = Group("grandparents")
 grandparents.add_user("Papa")
-grandparents.add_user("Mimi")
 
 parent1 = Group("parent1")
 grandparents.add_group(parent1)
@@ -56,96 +55,53 @@ grandkid1.add_user("Rebecca")
 
 parent2 = Group("parent2")
 grandparents.add_group(parent2)
-parent2.add_user("Wayne")
-parent2.add_user("Sue")
-grandkid2 = Group("grandkid2")
-parent2.add_group(grandkid2)
-grandkid2.add_user("Sarah")
-grandkid2.add_user("Melissa")
-grandkid2.add_user("Andrew")
+parent2.add_user("Wayne J")
 
 parent3 = Group("parent3")
 grandparents.add_group(parent3)
-parent3.add_user("Steve")
 parent3.add_user("Jenny")
 grandkid3 = Group("grandkid3")
 parent3.add_group(grandkid3)
 grandkid3.add_user("Kristina")
-grandkid3.add_user("Matthew")
 greatgrandkid1 = Group("greatgrandkid1")
 grandkid3.add_group(greatgrandkid1)
 greatgrandkid1.add_user("Jacob")
-greatgrandkid1.add_user("Veronica")
 
 parent4 = Group("parent4")
 grandparents.add_group(parent4)
-parent4.add_user("Stu")
-parent4.add_user("Cindy")
 grandkid4 = Group("grandkid4")
 parent4.add_group(grandkid4)
 grandkid4.add_user("Alex")
 grandkid4.add_user("Nate")
+grandkid4.add_user(5)
+grandkid4.add_user(None)
 
 parent5 = Group("parent5")
 grandparents.add_group(parent5)
-parent5.add_user("Brian")
 parent5.add_user("Lauri")
 grandkid5 = Group("grandkid5")
-parent5.add_group(grandkid5)
 grandkid5.add_user("Nick")
-grandkid5.add_user("Liz")
 
-# Print group structure
-print(grandparents)
-print(grandparents.groups[0])
-print(grandparents.groups[1])
-print(grandparents.groups[2])
-print(grandparents.groups[3])
-print(grandparents.groups[4])
-print(grandparents.groups[0].groups[0])
-print(grandparents.groups[1].groups[0])
-print(grandparents.groups[2].groups[0])
-print(grandparents.groups[3].groups[0])
-print(grandparents.groups[4].groups[0])
-print(grandparents.groups[2].groups[0].groups[0])
 
-people = ["Mimi","Papa", "Rod", "Suzzie", "Rebecca", "Wayne", "Sue", "Sarah", "Melissa","Andrew", "Steve", "Jenny",
-          "Kristina", "Matthew", "Jacob", "Veronica", "Stu", "Cindy", "Alex", "Nate", "Brian", "Lauri", "Nick",
-          "Liz", "Leigh", "Joanie", "Jill", "Pat", "Mickey", "Big Dog", "", "big-dog"]
+people = ["Papa", "Wayne J", "Jenny", "Kristina", "Jacob", "Alex", "Nate", "Lauri", "Nick", "Jill", "Big Dog", "", "big-dog", 5, None]
 
 for person in people:
     exist = is_user_in_group(person, grandparents)
     print("{} - {}".format(person, exist))
 
-# Mimi - True
+# Results:
 # Papa - True
-# Rod - True
-# Suzzie - True
-# Rebecca - True
-# Wayne - True
-# Sue - True
-# Sarah - True
-# Melissa - True
-# Andrew - True
-# Steve - True
+# Wayne J - True
 # Jenny - True
 # Kristina - True
-# Matthew - True
 # Jacob - True
-# Veronica - True
-# Stu - True
-# Cindy - True
 # Alex - True
 # Nate - True
-# Brian - True
 # Lauri - True
-# Nick - True
-# Liz - True
-# Leigh - False
-# Joanie - False
+# Nick - False    Not connected to grandparents group
 # Jill - False
-# Pat - False
-# Mickey - False
 # Big Dog - False
-#  - False             Edge case
-# big-dog - False      Edge case
+#  - False
+# big-dog - False
+# 5 - True
+# None - True
