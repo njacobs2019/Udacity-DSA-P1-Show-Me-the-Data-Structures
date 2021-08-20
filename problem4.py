716e77031b4898b4ea29826969e62297b7aafb33
Nicholas <nicholas.jacobs@maine.edu>
2021-08-19 22:28:03 -0400

    added if __name__=='__main__'

--- problem4.py
+++ problem4.py
@@ -42,66 +42,67 @@ def is_user_in_group(user, group):
         out = is_user_in_group(user, sub_group) or out
     return out
 
-grandparents = Group("grandparents")
-grandparents.add_user("Papa")
+if __name__ == '__main__':
+    grandparents = Group("grandparents")
+    grandparents.add_user("Papa")
 
-parent1 = Group("parent1")
-grandparents.add_group(parent1)
-parent1.add_user("Rod")
-parent1.add_user("Suzzie")
-grandkid1 = Group("grandkid1")
-parent1.add_group(grandkid1)
-grandkid1.add_user("Rebecca")
+    parent1 = Group("parent1")
+    grandparents.add_group(parent1)
+    parent1.add_user("Rod")
+    parent1.add_user("Suzzie")
+    grandkid1 = Group("grandkid1")
+    parent1.add_group(grandkid1)
+    grandkid1.add_user("Rebecca")
 
-parent2 = Group("parent2")
-grandparents.add_group(parent2)
-parent2.add_user("Wayne J")
+    parent2 = Group("parent2")
+    grandparents.add_group(parent2)
+    parent2.add_user("Wayne J")
 
-parent3 = Group("parent3")
-grandparents.add_group(parent3)
-parent3.add_user("Jenny")
-grandkid3 = Group("grandkid3")
-parent3.add_group(grandkid3)
-grandkid3.add_user("Kristina")
-greatgrandkid1 = Group("greatgrandkid1")
-grandkid3.add_group(greatgrandkid1)
-greatgrandkid1.add_user("Jacob")
+    parent3 = Group("parent3")
+    grandparents.add_group(parent3)
+    parent3.add_user("Jenny")
+    grandkid3 = Group("grandkid3")
+    parent3.add_group(grandkid3)
+    grandkid3.add_user("Kristina")
+    greatgrandkid1 = Group("greatgrandkid1")
+    grandkid3.add_group(greatgrandkid1)
+    greatgrandkid1.add_user("Jacob")
 
-parent4 = Group("parent4")
-grandparents.add_group(parent4)
-grandkid4 = Group("grandkid4")
-parent4.add_group(grandkid4)
-grandkid4.add_user("Alex")
-grandkid4.add_user("Nate")
-grandkid4.add_user(5)
-grandkid4.add_user(None)
+    parent4 = Group("parent4")
+    grandparents.add_group(parent4)
+    grandkid4 = Group("grandkid4")
+    parent4.add_group(grandkid4)
+    grandkid4.add_user("Alex")
+    grandkid4.add_user("Nate")
+    grandkid4.add_user(5)
+    grandkid4.add_user(None)
 
-parent5 = Group("parent5")
-grandparents.add_group(parent5)
-parent5.add_user("Lauri")
-grandkid5 = Group("grandkid5")
-grandkid5.add_user("Nick")
+    parent5 = Group("parent5")
+    grandparents.add_group(parent5)
+    parent5.add_user("Lauri")
+    grandkid5 = Group("grandkid5")
+    grandkid5.add_user("Nick")
 
 
-people = ["Papa", "Wayne J", "Jenny", "Kristina", "Jacob", "Alex", "Nate", "Lauri", "Nick", "Jill", "Big Dog", "", "big-dog", 5, None]
+    people = ["Papa", "Wayne J", "Jenny", "Kristina", "Jacob", "Alex", "Nate", "Lauri", "Nick", "Jill", "Big Dog", "", "big-dog", 5, None]
 
-for person in people:
-    exist = is_user_in_group(person, grandparents)
-    print("{} - {}".format(person, exist))
+    for person in people:
+        exist = is_user_in_group(person, grandparents)
+        print("{} - {}".format(person, exist))
 
-# Results:
-# Papa - True
-# Wayne J - True
-# Jenny - True
-# Kristina - True
-# Jacob - True
-# Alex - True
-# Nate - True
-# Lauri - True
-# Nick - False    Not connected to grandparents group
-# Jill - False
-# Big Dog - False
-#  - False
-# big-dog - False
-# 5 - True
-# None - True
+    # Results:
+    # Papa - True
+    # Wayne J - True
+    # Jenny - True
+    # Kristina - True
+    # Jacob - True
+    # Alex - True
+    # Nate - True
+    # Lauri - True
+    # Nick - False    Not connected to grandparents group
+    # Jill - False
+    # Big Dog - False
+    #  - False
+    # big-dog - False
+    # 5 - True
+    # None - True
