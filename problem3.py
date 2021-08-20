239367f7fee068dd17425480de1458bf4f370440
Nicholas <nicholas.jacobs@maine.edu>
2021-08-19 22:00:50 -0400

    Fixed edge cases and now allows for repetetive single characters and non-string objects

--- problem3.py
+++ problem3.py
@@ -212,10 +212,16 @@ def _gen_forward_map(root, mapping, sequence):
         right_sequence = sequence+"1"
         mapping = _gen_forward_map(root.right, mapping, right_sequence)
     
+    if len(sequence)==0:
+        sequence = sequence+"0"
     mapping[root.char]=sequence
     return mapping
 
 def huffman_encoding(data):
+    if not isinstance(data, str):
+        data = str(data)
+        print("Warning:  data to be encoded was converted to a string")
+
     frequencies = calc_frequencies(data)
     tree = Tree()
 
@@ -253,34 +259,19 @@ def huffman_decoding(data,root):
     current = root
 
     for bit in data:
-        if bit=='0':
-            current = current.left
-        else:
-            current = current.right
-
+        if not current.char:
+            if bit=='0':
+                current = current.left
+            else:
+                current = current.right
 
         # Case when at leaf node
         if current.char:
             decoded += current.char
             current=root
 
-
     return decoded
 
-# if __name__ == '__main__':
-#     data = "TheBirdIsTheWord"
-#     print("Input: {}".format(data))
-
-#     compressed, root = huffman_encoding(data)
-
-#     print("Compressed: {}".format(compressed))
-
-#     huff_tree = HuffTree(root)
-#     print(huff_tree)
-
-#     decoded = huffman_decoding(compressed, root)
-
-#     print("\nDecoded: {}".format(decoded))
 
 if __name__ == "__main__":
     
@@ -290,6 +281,9 @@ if __name__ == "__main__":
     sentences.append("Hello")
     sentences.append("!?")
     sentences.append("54")
+    sentences.append("AAAAAAAA")
+    sentences.append(5)
+    sentences.append(None)
 
     for sentence in sentences:
         print("--"*10)
@@ -303,3 +297,86 @@ if __name__ == "__main__":
         decoded_data = huffman_decoding(encoded_data, tree)
         print ("The size of the decoded data is: {}".format(sys.getsizeof(decoded_data)))
         print ("The content of the encoded data is: {}\n".format(decoded_data))
+
+# Output of testing:
+# --------------------
+# The size of the data is: 69
+# The content of the data is: The bird is the word
+
+# The size of the encoded data is: 36
+# The content of the encoded data is: 1100001101010110010000011101100001111110111000110101011101111100011101
+
+# The size of the decoded data is: 69
+# The content of the encoded data is: The bird is the word
+
+# --------------------
+# The size of the data is: 521
+# The content of the data is: Sebastian Thrun (born May 14, 1967) is an entrepreneur, educator, and computer scientist from Germany. He is CEO of Kitty Hawk Corporation, and chairman and co-founder of Udacity. Before that, he was a Google VP and Fellow, a Professor of Computer Science at Stanford University, and before that at Carnegie Mellon University. At Google, he founded Google X and Google's self-driving car team. He is also an Adjunct Professor at Stanford University and at Georgia Tech.[4]
+
+# The size of the encoded data is: 320
+# The content of the encoded data is: 10011110001101111011001101001010010110010001111010111001110001101010101000111100101001110111101011011010001110111100111000111011110111101001111011011111111011110101001011011001011111001011001001011101110010110101111100100011100010000101011000010011010110000100000010101001100111111110001010010101000110110001011011011001111111111001000101001110011010110100001001101101010010100001101111101000110001000010000101001011010010111101001011010110100001110011110000110010000110010000111011101110111001110000011100101101011110011101001000111001001101111011010011111001000010010010101010111011110011100110011011111101011110111100111010110110100110110110110110001010010101110000111111111100100010100111001100111001100001001100100001100100011111001000101001110011010111001010101001101110101010001010000001101111011010011111001100101001100001100010010101110111011101111001000100000100110110110000111010101110011000101011111111011100000111110111111100110101111100111001111101110110100011101100001111001001110011101111110010001010011110010000000011011011011010111101111101111111111001110011101011010110100100011010110101011011011110110100111110011101011010000100110110101001010000110111100111100110001000010000011000011111000101111100111101011100100001001101101101010011110011001000001010101100000110110100010010101110101111111111001000101001111101111000001001101101100001110101011100110001011111100010111110011101100011010000000100010010000111011110010001101101101101011100011110011001000001010101100000110110100010010101110111011101110111100001011110011111011101101000111011000001111111101110000011101001101110101010001010000010100111001111101110110100011101100001111001001001111100100010100111001111101110110100011101100001001010001101011111010000110110010011001010110100011000101010110001010000100011110011011000110111010100011000100001101110111001110000011100101101011111001101101101010111111100100011101111000101001010111111101010100000110010111100111010110101101001000110101101010110110111110001011111001111010111001000010011011011010100111100110010000010101011000001101101000100101011101111110010001010011111000101111001111000101101100100010010110011110101110000001100111001101110100100101011110111010111110
+
+# The size of the decoded data is: 521
+# The content of the encoded data is: Sebastian Thrun (born May 14, 1967) is an entrepreneur, educator, and computer scientist from Germany. He is CEO of Kitty Hawk Corporation, and chairman and co-founder of Udacity. Before that, he was a Google VP and Fellow, a Professor of Computer Science at Stanford University, and before that at Carnegie Mellon University. At Google, he founded Google X and Google's self-driving car team. He is also an Adjunct Professor at Stanford University and at Georgia Tech.[4]
+
+# --------------------
+# The size of the data is: 54
+# The content of the data is: Hello
+
+# The size of the encoded data is: 28
+# The content of the encoded data is: 1101110010
+
+# The size of the decoded data is: 54
+# The content of the encoded data is: Hello
+
+# --------------------
+# The size of the data is: 51
+# The content of the data is: !?
+
+# The size of the encoded data is: 28
+# The content of the encoded data is: 01
+
+# The size of the decoded data is: 51
+# The content of the encoded data is: !?
+
+# --------------------
+# The size of the data is: 51
+# The content of the data is: 54
+
+# The size of the encoded data is: 28
+# The content of the encoded data is: 10
+
+# The size of the decoded data is: 51
+# The content of the encoded data is: 54
+
+# --------------------
+# The size of the data is: 57
+# The content of the data is: AAAAAAAA
+
+# The size of the encoded data is: 24
+# The content of the encoded data is: 00000000
+
+# The size of the decoded data is: 57
+# The content of the encoded data is: AAAAAAAA
+
+# --------------------
+# The size of the data is: 28
+# The content of the data is: 5
+
+# Warning:  data to be encoded was converted to a string
+# The size of the encoded data is: 24
+# The content of the encoded data is: 0
+
+# The size of the decoded data is: 50
+# The content of the encoded data is: 5
+
+# --------------------
+# The size of the data is: 16
+# The content of the data is: None
+
+# Warning:  data to be encoded was converted to a string
+# The size of the encoded data is: 28
+# The content of the encoded data is: 10010011
+
+# The size of the decoded data is: 53
+# The content of the encoded data is: None
